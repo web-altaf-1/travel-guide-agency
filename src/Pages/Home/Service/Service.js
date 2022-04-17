710185c1b6325906ee4cfd9b2cbff2ba98a69ad0
web-altaf-1 <web.altaf.1@gmail.com>
2022-04-18 01:18:24 +0600

    service components was added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Service.css';
 
-const Service = () => {
+const Service = ({ service }) => {
+    const { name, img, price, description } = service;
     return (
-        <div>
-            <h2>This is service </h2>
+        <div className='service shadow-lg mb-5'>
+            <img className='w-100' src={img} alt="" />
+            <div style={{height:'270px'}}>
+                <h3 className='service-name'>{name}</h3>
+                <h5>Price: ${price}</h5>
+                <p className=''><small> {description}</small></p>
+            </div>
+            <button className='btn btn-primary mt-3'>Book Now</button>
         </div>
     );
 };
