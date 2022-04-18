0f430ecfd400e72d3e1e6806cf9811c3f0e3b05c
web-altaf-1 <web.altaf.1@gmail.com>
2022-04-18 23:41:39 +0600

    cheakout page form added

--- src/Pages/Cheakout/Cheakout.js
+++ src/Pages/Cheakout/Cheakout.js
@@ -3,8 +3,13 @@ import './Cheakout.css';
 
 const Cheakout = () => {
     return (
-        <div>
-            <h1>Please Cheakout</h1>
+        <div className='w-75 mx-auto shadow-lg p-5'>
+            <form className='' >
+                <input className='w-50 mb-2 mx-auto d-block px-2' type="text"  placeholder='Enter Your Name'/> <br />
+                <input className='w-50 mb-2 mx-auto d-block px-2' type="text" placeholder='Enter Your Address' /> <br />
+                <input className='w-50 mb-2 mx-auto d-block px-2' type="number" name="" id="" placeholder='Enter your Phone Number ' /> <br />
+                <input className='w-25 mb-2 mx-auto d-block  p-3'  type="submit" value="Go For Payment" />
+            </form>
         </div>
     );
 };
